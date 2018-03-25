0346265d7cdca060c033cae784f876fb26df1ad7
dbraghis <danbraghis@yahoo.com>
2018-03-25 14:03:08 +0300

    fixed sending

--- php/send.php
+++ php/send.php
@@ -4,9 +4,9 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
 
-$to = 'youremail@gmail.com';
+$to = 'danbraghis@yahoo.com';
 $message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
-$headers = 'From: danbraghis@yahoo.com' . "\r\n";
+$headers = 'From: yourname@domain.com' . "\r\n";
  
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
 mail($to, $subject, $message, $headers); //This method sends the mail.
